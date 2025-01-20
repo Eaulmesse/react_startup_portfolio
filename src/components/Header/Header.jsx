5acf5bfd30f5a440dcbbac5aa695e6f94976bc62
Eaulmesse <theo.guerin01120@gmail.com>
2025-01-20 20:35:11 +0100

    edit: solving toggleMenu Mobile navigation issue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 
     return (
         <header>
-            <h1 className='w-4/5  m-auto text-center poppins-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text text-4xl md:text-6xl lg:text-7xl py-3 mt-5 lg:mt-10'>Développeur Front-End pour Startups : MVP rapides et interfaces engageantes."</h1>
+            <h1 className='w-4/5 m-auto text-center poppins-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text text-4xl md:text-6xl lg:text-7xl py-3 mt-5 lg:mt-20'>Développeur Front-End pour Startups : MVP rapides et interfaces engageantes."</h1>
             <p className='w-4/5 lg:w-3/5 poppins-regular text-center m-auto text-xl text-neutral-500'>Je conçois des applications intuitives, réactives et optimisées pour offrir une expérience utilisateur fluide, tout en respectant les délais serrés des startups ambitieuses.</p>
 
             <div className='flex justify-center h-fit'>
--- src/components/misc/OfferButton.jsx
+++ src/components/misc/OfferButton.jsx
@@ -5,7 +5,7 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
 
 const OfferButton = ({ label, onClick }) => {
     return (
-        <button onClick={onClick} className="px-5 w-44 poppins-regular m-auto mt-10 bg-purple-600 text-lg py-2 rounded-full flex justify-between items-center">
+        <button onClick={onClick} className="px-5 w-44 poppins-regular m-auto mt-10 bg-purple-600 text-lg py-2 rounded-full flex justify-between items-center shadow shadow-purple-600 hover:shadow-purple-800 text-purple-100">
             <FontAwesomeIcon icon={faMeteor} />
             {label}
             <FontAwesomeIcon icon={faArrowRight} />
