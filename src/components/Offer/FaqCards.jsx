7b72b499a6f2136e7adf5269f220bb471f8c3302
Eaulmesse <theo.guerin01120@gmail.com>
2025-01-29 10:55:30 +0100

    edit: changing the FaqCard div by a button

--- src/components/Offer/FaqCards.jsx
+++ src/components/Offer/FaqCards.jsx
@@ -14,12 +14,12 @@ const FaqCards = ({ title, description }) => {
     return (
         <div className="mt-5">
     
-            <div onClick={handleClick} className='w-full bg-neutral-900 p-5 rounded-2xl flex items-center justify-between'>
+            <button onClick={handleClick} className='w-full bg-neutral-900 p-5 rounded-2xl flex items-center justify-between cursor-pointer'>
                 <p className='text-white poppins-medium text-sm lg:text-lg'>{title}</p>
                 <ChevronRight className={`text-white w-8 h-8 ${DisplayText ? "hidden" : "block"} `} />
                 <ChevronDown className={`text-white w-8 h-8 ${DisplayText ? "block" : "hidden"} `}  />
 
-            </div>
+            </button>
 
             <p className={`text-white text-sm lg:text-base poppins-regular mt-2 w-[95%] m-auto ${DisplayText ? "block" : "hidden" } `} >{description}</p>
         </div>
