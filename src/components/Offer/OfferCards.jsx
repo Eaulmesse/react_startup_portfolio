ca1e66b40c7c1861ca16176d664aa5154939613a
Eaulmesse <theo.guerin01120@gmail.com>
2025-01-29 10:49:27 +0100

    edit: adding variants framer motion

--- src/components/Offer/OfferCards.jsx
+++ src/components/Offer/OfferCards.jsx
@@ -21,12 +21,12 @@ const OfferCards = ({ title, description }) => {
     };
 
   return (
-    <motion.div className="max-w-[256px] w-full flex flex-col items-center justify-center bg-neutral-900 p-5 rounded-2xl relative overflow-hidden m-5"  whileHover="hover" initial="initial" >
+    <motion.div className="max-w-[256px] w-full flex flex-col items-center justify-center bg-neutral-900 p-5 rounded-2xl relative overflow-hidden m-5"  whileHover="hover" initial="initial" variants={parentVariants}>
 
         <p className="w-5/6 text-white poppins-bold text-xl lg:text-2xl">{title}</p>
         <p className="w-5/6 text-white poppins-medium text-sm mt-5">{description}</p>
 
-        <motion.div whileInView={{ height: isXL ? "320px" : "40px"}} className="w-80 h-96 -rotate-6 bg-gradient-to-b from-pink-500 to-purple-500 absolute -bottom-5">
+        <motion.div whileInView={{ height: isXL ? "320px" : "40px"}} className="w-80 h-96 -rotate-6 bg-gradient-to-b from-pink-500 to-purple-500 absolute -bottom-5" variants={childVariants}>
             <p className="text-center text-white poppins-medium hidden lg:block">Découvrez la carte</p>
         </motion.div>
       
