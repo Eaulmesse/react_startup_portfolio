0ab7242f100d96d5f981f2f74e77d17446d20bf4
Eaulmesse <theo.guerin01120@gmail.com>
2025-02-05 17:52:30 +0100

    edit: changing react-router by hashRouter to use gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react_startup_portfolio"
+  base: "/react_startup_portfolio/"
 })
